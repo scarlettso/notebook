6714b2e2e51fed89c5c614559e49350b7e14c274
scarlettso <u3557171@connect.hku.hk>
2022-03-30 21:26:50 +0800

    server-testing

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ mongoose
         app.use(cors());
         app.use(express.json())
         app.use("/api", router)
+        app.use(express.static('react-part/public'))
 
-        app.listen(5000, () => {
+        app.listen(process.env.PORT || 5000, () => {
             console.log("Server has started!")
         })
+
+        
     }
     
     )
