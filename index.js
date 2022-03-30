9c83b2cf7fbe0f576b319fbf8748166e47da1e5a
scarlettso <u3557171@connect.hku.hk>
2022-03-30 21:51:08 +0800

    api bug fixed

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ mongoose
         app.use(cors());
         app.use(express.json())
         app.use("/api", router)
+        app.get("/checklist", express.static('react-part/build'))
         app.use(express.static('react-part/build'))
+        
 
         app.listen(process.env.PORT || 5000, () => {
             console.log("Server has started!")
         })
 
-        app.get("/checklist", express.static('react-part/build'))
+        
 
         
     }
