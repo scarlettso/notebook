19d09d7f69f9ddb21231de1c7d23fcf95972e581
scarlettso <u3557171@connect.hku.hk>
2022-03-30 21:33:56 +0800

    build page

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose
         app.use(cors());
         app.use(express.json())
         app.use("/api", router)
-        app.use(express.static('react-part/public'))
+        app.use(express.static('react-part/build'))
 
         app.listen(process.env.PORT || 5000, () => {
             console.log("Server has started!")
